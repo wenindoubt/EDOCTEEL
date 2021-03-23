18f7746283110f1e70ab4af2d7d680ab7a7b738d
Jeffrey Wen <jeffrey@wenindoubt.com>
2021-03-23 00:46:01 -0700

    Create recursive-preorder-traversal.java

---
+++ March/recursive-preorder-traversal.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        return dfs(root, new ArrayList<Integer>());
+    }
+    
+    private List<Integer> dfs(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return result;
+        }
+        
+        result.add(root.val);
+        dfs(root.left, result);
+        dfs(root.right, result);
+        
+        return result;
+    }
+}
+
+// Time Complexity is O(n), where n is the number of nodes in the tree.
+// We visit each node exactly once.
+
+// Space complexity is O(h), where h is the height of the tree.
+// In this case, O(h) = O(n).
+
+// If this was a balanced binary search tree, space complexity would
+// be equal to O(log(n)), where n is the number of nodes in the tree.
