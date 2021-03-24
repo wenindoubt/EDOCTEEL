c7f45b698fc0b3da259f4267e3e4cd45bf767252
Jeffrey Wen <jeffrey@wenindoubt.com>
2021-03-24 09:00:10 -0700

    Create iterative-preorder-traversal.java

---
+++ March/iterative-preorder-traversal.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+        
+        while (current != null | !stack.isEmpty()) {
+            while (current != null) {
+                result.add(current.val);
+                stack.push(current);
+                current = current.left;
+            }
+            current = stack.pop();
+            current = current.right;
+        }
+        
+        return result;
+    }
+}
+
+// Time Complexity is O(n), where n is the number of nodes in the tree.
+// We visit each node exactly once.
+
+// Space complexity is O(h), where h is the height of the tree.
+// In this case, O(h) = O(n).
+
+// If this was a balanced binary search tree, space complexity would
+// be equal to O(log(n)), where n is the number of nodes in the tree.
