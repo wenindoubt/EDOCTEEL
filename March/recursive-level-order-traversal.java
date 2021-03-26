68baceab4072168f475aabaed8f33bed6e12767a
Jeffrey Wen <jeffrey@wenindoubt.com>
2021-03-26 13:23:39 -0700

    Update recursive-level-order-traversal.java

--- March/recursive-level-order-traversal.java
+++ March/recursive-level-order-traversal.java
@@ -33,3 +33,12 @@ class Solution {
         return result;
     }
 }
+
+// Time Complexity is O(n), where n is the number of nodes in the tree.
+// We visit each node exactly once.
+
+// Space complexity is O(h), where h is the height of the tree.
+// In this case, O(h) = O(n).
+
+// If this was a balanced binary search tree, space complexity would
+// be equal to O(log(n)), where n is the number of nodes in the tree.
