fa0f71a0c4b7b1267017b97c3f73c170d5cedd52
Jeffrey Wen <jeffrey@wenindoubt.com>
2021-03-26 12:20:21 -0700

    Create recursive-level-order-traversal.java

---
+++ March/recursive-level-order-traversal.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        
+        if (root == null) {
+            return result;
+        }
+        
+        return dfs_preorder(root, result, 0);
+    }
+    
+    private List<List<Integer>> dfs_preorder(TreeNode root, List<List<Integer>> result, int level) {
+        
+        System.out.println("result.size() is: " + result.size());
+        
+        // if the level that we're on is the same as the size of the array
+        // that means its our first time on this level and we should create a new array
+        // to support adding new values
+        if (result.size() == level) {
+            result.add(new ArrayList<Integer>());
+        }
+        
+        result.get(level).add(root.val);
+        
+        if (root.left != null) {
+            dfs_preorder(root.left, result, level + 1);
+        }
+        
+        if (root.right != null) {
+            dfs_preorder(root.right, result, level + 1);
+        }
+        
+        return result;
+    }
+}
