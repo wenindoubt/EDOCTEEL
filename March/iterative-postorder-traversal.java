4999302dd47d57ec63027d56b0e836e5bd555781
Jeffrey Wen <jeffrey@wenindoubt.com>
2021-04-01 01:42:25 -0700

    Update iterative-postorder-traversal.java

--- March/iterative-postorder-traversal.java
+++ March/iterative-postorder-traversal.java
@@ -1,28 +1,32 @@
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         LinkedList<Integer> result = new LinkedList<>();
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode current = root;
-
-        if (current == null) {
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        TreeNode currentNode = root;
+        
+        if (currentNode == null) {
             return result;
         }
-
-        stack.push(current);
-
+        
+        stack.push(currentNode);
+        
         while (!stack.isEmpty()) {
-            current = stack.pop();
-            result.addFirst(current.val);
-
-            if (current.left != null) {
-                stack.add(current.left);
+            currentNode = stack.pop();
+            result.push(currentNode.val);
+            
+            if (currentNode.left != null) {
+                stack.push(currentNode.left);
             }
-
-            if (current.right != null) {
-                stack.add(current.right);
+            
+            if (currentNode.right != null) {
+                stack.push(currentNode.right);
             }
         }
-
+        
         return result;
     }
 }
+
+// Time complexity is O(n) where n is the number of nodes that we have to visit.
+
+// Space complexity is O(n) where n is the size of the stack.
